2de90c3580d3bd098090a64a47bef056beba6c19
elizabeth.meyer25 <elizabeth.meyer25@gmail.com>
2024-02-08 11:29:27 -0600

    8-11.5 done, almost done with 12

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+// ****************************************************************
+// DogTest.java
+//
+// A simple test class that creates a Dog and makes it speak.
+//
+// ****************************************************************
+
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Dog dog = new Dog("Spike");
+        System.out.println(dog.getName() + " says " + dog.bark());
+
+        Labrador lab = new Labrador("Happy", "golden");
+        System.out.println(lab.getName());
+        System.out.println(lab.bark());
+        //it gets bark() from the dog class, inheritance
+
+        Yorkshire york = new Yorkshire("NewYork", 5);
+        york.waddle();
+        // lab.waddle(); doesn't work! waddle is in Yorkshire, not Dog, and Lab+York are siblings
+        lab.waddle(); //fixed!
+
+        Dog[] dogs = new Dog[3];
+        Dog yogi = new Yorkshire("Yogi", 7);
+        dogs[1] = yogi;
+        Dog doug = new Dog("Dodger");
+        dogs[0] = doug;
+        Dog cherby = new Labrador("Cherry", "black");
+        dogs[2] = cherby;
+
+//        for (int x = 0; x < 3; x++) {
+//            System.out.println(dogs[x].bark());
+//        }
+
+
+//        for (Dog goobs: dogs) {
+//            System.out.println(goobs.bark());
+//        }
+        //dont have to cast to call bark bc its in Dog and everyone has a bark method in the parent class.
+        //you have to cast the labrador to a labrador because you're calling it from the array, and it's currently being stored as a dog to make it fit into the dog array
+        ((Labrador)dogs[2]).waddle();
+
+        ArrayList<Dog> doggies = new ArrayList<Dog>();
+        doggies.add(new Dog("Charlie Puth"));
+        doggies.add(new Yorkshire("Ed Sheeran", 5));
+        doggies.add(new Labrador("Golden Freddy", "gold"));
+        for(int x = 0; x < 3; x++){
+            System.out.println(dogs[x].bark());
+        }
+
+
+
+
+
+
+    }
+}
